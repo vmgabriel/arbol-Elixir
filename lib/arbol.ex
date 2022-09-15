b267017499d2435f46069df91890c931e52b5b4c
Gabriel <vmgabriel96@gmail.com>
2022-09-14 19:50:44 -0500

    feat: change in tree with documentation and unit test

--- lib/arbol.ex
+++ lib/arbol.ex
@@ -1,41 +1,99 @@
-defmodule Arbol do
+defmodule Tree do
+  @moduledoc """
+  Struct of Module, this allo left, right and val
 
-  defstruct [:izq, :der, :val]
+  this have functions related to change in tree behaviour
+  """
 
-  def nuevo(valor) do
-    agregar(nil, valor)
+  defstruct [
+    :val,
+    left: nil,
+    right: nil
+  ]
+
+  @doc """
+  return new tree with val equal the value get
+
+  ### Examples
+      iex> Tree.new(10)
+      %Tree{left: nil, right: nil, val: 10}
+  """
+  def new(val) do
+    struct(__MODULE__, val: val)
+  end
+end
+
+
+defmodule TreeBalanced.Core do
+  @moduledoc """
+  Core of Tree Balanced this have function related to use of tree and re-order
+  """
+
+  alias Tree
+
+  @doc """
+  add to tree value or create new tree with value
+
+  ### Examples
+      iex> TreeBalanced.Core.add(nil, 10)
+      %Tree{left: nil, right: nil, val: 10}
+      iex> TreeBalanced.Core.add(TreeBalanced.Core.add(nil, 10), 20)
+      %Tree{left: nil, right: %Tree{left: nil, right: nil, val: 20}, val: 10}
+
+  """
+  def add(nil, val), do: Tree.new(val)
+  def add(%Tree{left: left, val: intern_val, right: right} = _tree, val) when intern_val <= val do
+    %Tree{left: left, right: add(right, val), val: intern_val}
+  end
+  def add(%Tree{left: left, val: intern_val, right: right} = _tree, val) do
+    %Tree{left: add(left, val), right: right, val: intern_val}
   end
 
-  def agregar(_, valor) when is_nil(valor) do
-    IO.puts "Valor no ha sido dado"
-  end
+  @doc """
+  Check in tree count of nodes if tree is nil return zero
 
-  def agregar(arbol, valor) do
-    if is_nil(arbol) do
-      %Arbol{val: valor}
-    else
-      %Arbol{izq: izq, der: der, val: val} = arbol
-      IO.puts "valor: #{val==nil}"
-      if val == nil do
-        %Arbol{val: valor}
-      else
-        if valor >= val do
-          # Es mayor o igual
-          %Arbol{izq: izq, der: agregar(der,valor), val: val}
-        else
-          # Es menor o igual
-          %Arbol{izq: agregar(izq,valor), der: der, val: val}
-        end
-      end
-    end
-  end
+  ### Examples
+      iex> TreeBalanced.Core.countNodes(TreeBalanced.Core.add(TreeBalanced.Core.add(nil, 10), 20))
+      2
 
-  def cantidadNodos(arbol) do
-    if is_nil(arbol) do
-      0
-    else
-      %Arbol{izq: izq, der: der} = arbol
-      1 + cantidadNodos(izq) + cantidadNodos(der)
-    end
+  """
+  def countNodes(nil), do: 0
+  def countNodes(%Tree{left: left, right: right} = _tree) do
+    1 + countNodes(left) + countNodes(right)
   end
+end
+
+
+defmodule TreeBalanced do
+  @moduledoc """
+  Tree Balanced Fachade for Functions
+
+  this have following common functions:
+    - add/2 -> add val as node in tree
+    - countNodes/1 -> count the value of current tree
+  """
+
+  alias TreeBalanced.Core
+
+  @doc """
+  add to tree value or create new tree with value
+
+  ### Examples
+      iex> TreeBalanced.add(10)
+      %Tree{left: nil, right: nil, val: 10}
+      iex> TreeBalanced.add(20, TreeBalanced.add(10))
+      %Tree{left: nil, right: %Tree{left: nil, right: nil, val: 20}, val: 10}
+
+  """
+  def add(val, tree \\ nil), do: Core.add(tree, val)
+
+  @doc """
+  Check in tree count of nodes if tree is nil return zero
+
+  ### Examples
+      iex> TreeBalanced.countNodes(TreeBalanced.add(20, TreeBalanced.add(10)))
+      2
+
+  """
+  def countNodes(tree), do: Core.countNodes(tree)
 end
--- test/arbol_test.exs
+++ test/arbol_test.exs
@@ -1,8 +1,23 @@
-defmodule ArbolTest do
+defmodule TreeBalancedTest do
   use ExUnit.Case
-  doctest Arbol
+  doctest TreeBalanced
+  doctest TreeBalanced.Core
+  doctest Tree
 
-  test "greets the world" do
-    assert Arbol.hello() == :world
+  describe "add/2" do
+    test "Add in Balanced Tree" do
+      assert 2 == 2
+    end
+  end
+
+  describe "countNodes/1" do
+    test "Check Count Nodes" do
+      assert TreeBalanced.countNodes(TreeBalanced.add(10)) == 1
+
+      nodes = [10, 20, 30, 40]
+      count_nodes = Enum.count(nodes)
+      t = Enum.reduce(nodes, nil, fn node,tree -> TreeBalanced.add(node, tree) end)
+      assert TreeBalanced.countNodes(t) == count_nodes
+    end
   end
 end
